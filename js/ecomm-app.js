1b0dab04a8908d26848ddd92a3d3648469348b7f
Asterisk Loftis <asterisk@asteriskloftis.com>
2016-04-26 15:47:26 -0600

    Initial Commit
    Simple prototype for a responsive banner with a interesting gradient
    slice over a background image.

---
+++ js/ecomm-app.js
@@ -0,0 +1,21 @@
+﻿angular.module('ecommDirectives', [])
+.directive('ecommHeader', function () {
+    return {
+        restrict: 'AE',
+        templateUrl: '../partials/_header.html'
+    };
+})
+.directive('ecommFooter', function () {
+    return {
+        restrict: 'AE',
+        templateUrl: '../partials/_footer.html'
+    };
+});
+
+$(document).ready(function () {
+
+    // init showing interchange images inside orbit
+    $('[data-orbit]').on('replace', 'img', function (e, new_path, original_path) {
+        $('[data-orbit]').foundation('orbit').resize();
+    });
+});
